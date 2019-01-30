5ec8dce8262ff7ed0d33d280ed21b2cf97c20531
reedliu <jieandze1314@gmail.com>
2019-01-30 22:46:13 +0800

    add content

--- one-key-blast/run_blast.sh
+++ one-key-blast/run_blast.sh
@@ -1,6 +1,3 @@
-#################################
-### Created by Reed Liu on 19.1.27
-#################################
 #!/bin/bash
  
 ######################################################################
@@ -30,17 +27,19 @@ REV=`tput smso`
  
 #Help function
 function HELP {
+  echo -e "***CREATED BY Yunze Liu (jieandze1314@gmail.com)***"
   echo -e \\n"Help documentation for ${BOLD}${SCRIPT}.${NORM}"\\n
-  echo -e "${REV}Basic usage:${NORM} ${BOLD}$SCRIPT file.ext${NORM}"\\n
   echo "Command line switches are optional. The following switches are recognized."
-  echo "${REV}-e${NORM}  --Sets TRUE/FALSE for extraction of fasta ${BOLD}e${NORM}. Default is ${BOLD}FALSE${NORM}."
-  echo "${REV}-S${NORM}  --If sets -e yes, you can set ${BOLD}start${NORM} position. Default is ${BOLD}0${NORM}."
-  echo "${REV}-E${NORM}  --If sets -e yes, you can set ${BOLD}end${NORM} position. "
-  echo "${REV}-L${NORM}  --If sets -e yes, you can set ${BOLD}length${NORM} for each line. Default is ${BOLD}100${NORM}."
   echo "${REV}-t${NORM}  --Sets the type of blast database ${BOLD}ncbidb/owndb${NORM}. Default is ${BOLD}ncbidb${NORM}."
+  echo "${REV}-T${NORM}  --If sets -t owndb,you can build your own database with ${BOLD}Target fasta sequence${NORM}."
   echo "${REV}-c${NORM}  --Sets the cpu number for option ${BOLD}c${NORM}. Default is ${BOLD}5${NORM}."
+  echo "${REV}-e${NORM}  --Sets ${BOLD}yes/no${NORM} for extraction. Default is ${BOLD}no${NORM}."
+  echo "${REV}-S${NORM}  --If sets -e TRUE, you can set ${BOLD}start${NORM} position. Default is ${BOLD}0${NORM}."
+  echo "${REV}-E${NORM}  --If sets -e TRUE, you can set ${BOLD}end${NORM} position. "
+  echo "${REV}-L${NORM}  --If sets -e TRUE, you can set ${BOLD}length${NORM} for each line. Default is ${BOLD}100${NORM}."
   echo -e "${REV}-h${NORM}  --Displays this help message."\\n
-  echo -e "Example: ${BOLD}$SCRIPT -t ncbidb -c 5 -e TRUE -S 0 -E 600 -L 100 ${NORM}"\\n
+  echo -e "Example 1: ${BOLD}$SCRIPT -t ncbidb -c 5 -e yes -S 0 -E 600 -L 100 ${NORM}"\\n
+  echo -e "Example 2: ${BOLD}$SCRIPT -t owndb -T /home/ncbi/database/owndb/own.fasta -c 3 -e no  ${NORM}"\\n
   exit 1
 }
  
@@ -55,17 +54,22 @@ fi
  
 #Parse command line flags
 type='ncbidb'
+target=''
 cpu=5
-extract=FALSE
+extract=yes
 start=0
 end=''
 length=100
+
  
-while getopts :t:c:e:S:E:L:h FLAG; do
+while getopts :t:T:c:e:S:E:L:h FLAG; do
   case $FLAG in
     t)  #set option "t"
       type=$OPTARG
       ;;
+    T)  #set option "T"
+      target=$OPTARG
+      ;;      
     c)  #set option "c"
       cpu=$OPTARG
       ;;
@@ -87,9 +91,6 @@ while getopts :t:c:e:S:E:L:h FLAG; do
     \?) #unrecognized option - show help
       echo -e \\n"Option -${BOLD}$OPTARG${NORM} not allowed."
       HELP
-      #在这里如果你不想打印完整的帮助信息，只想显示简单的错误信息，去掉上面的两行，同时使用下面的两行。
-      #echo -e "Use ${BOLD}$SCRIPT -h${NORM} to see the help documentation."\\n
-      #exit 2
       ;;
   esac
 done
@@ -97,7 +98,6 @@ done
 shift $((OPTIND-1))  #This tells getopts to move on to the next argument.
  
 ### End getopts code ###
-
 
 
 cat<<EOF >rename_seq.sh
@@ -155,51 +155,102 @@ while (<FA>){
 }
 EOF
 
-if [ "$extract" == "TRUE" ]; then
-	perl extract_seq.pl all.fasta $start $end $length > all.edit.fasta
-	if [ "$type" == "ncbidb" ]; then
-		blastn -query all.edit.fasta -db nt -outfmt 10 \
-			-out "all.edit.ncbidb.csv" -evalue 1e-5 \
-			-perc_identity 99 -num_alignments 10 -num_threads $cpu
-	else
-		blastn -query all.edit.fasta -db owndb -outfmt 10 \
-			-out "all.edit.owndb.csv" -num_threads $cpu
-	fi
-else
-	if [ "$type" == "ncbidb" ]; then
-		blastn -query all.fasta -db nt -outfmt 10 \
-			-out "all.ncbidb.csv" -evalue 1e-5 \
-			-perc_identity 99 -num_alignments 10 -num_threads $cpu
-	else
-		blastn -query all.fasta -db owndb -outfmt 10 \
-			-out "all.owndb.csv" -num_threads $cpu
-	fi
 
+
+
+if [ "$extract" == "yes" ]; then
+	perl extract_seq.pl all.fasta $start $end $length > all.edit.fasta
+	if [ "$type" == "owndb" ]; then
+    dir=`dirname $target`
+    makeblastdb -in $target -dbtype nucl -parse_seqids -out ${dir}/owndb
+    blastn -query all.edit.fasta -db ${dir}/owndb \
+      -outfmt 7 \
+      -out "all_edit_owndb.csv" -num_threads $cpu
+	elif [ "$type" == "ncbidb" ]; then
+    blastn -query all.edit.fasta -db nt \
+      -outfmt '7 qseqid sseqid sscinames scomnames pident length mismatch gapopen qstart qend sstart send evalue bitscore' \
+      -out "all_edit_ncbidb.csv" -evalue 1e-5 \
+      -perc_identity 99 -num_alignments 10 -num_threads $cpu
+  else
+    echo "** Please check the argument! **"
+	fi
+elif [ "$extract" == "no" ]; then
+	if [ "$type" == "owndb" ]; then
+    dir=`dirname $target`
+    makeblastdb -in $target -dbtype nucl -parse_seqids -out ${dir}/owndb
+    blastn -query all.fasta -db ${dir}/owndb \
+      -outfmt 7 \
+      -out "all_owndb.csv" -num_threads $cpu
+  elif [ "$type" == "ncbidb" ]; then
+    blastn -query all.fasta -db nt \
+      -outfmt '7 qseqid sseqid sscinames scomnames pident length mismatch gapopen qstart qend sstart send evalue bitscore' \
+      -out "all_ncbidb.csv" -evalue 1e-5 \
+      -perc_identity 99 -num_alignments 10 -num_threads $cpu
+  else
+    echo "** Please check the argument! **"
+  fi
+else
+  echo "** Please check the argument! **"
 fi
 
 
-
-cat<<EOF >tidy_result.r
+cat<<EOF >extract_owndb_result.r
 options(stringsAsFactors = FALSE)
-if(file.exists("all.edit.ncbidb.csv")){
-	x <- read.csv("all.edit.ncbidb.csv")
-}
-elif(file.exists("all.edit.owndb.csv")){
-	x <- read.csv("all.edit.owndb.csv")
-}
-elif(file.exists("all.ncbidb.csv")){
-	x <- read.csv("all.ncbidb.csv")
-}
-else(x <- read.csv("all.owndb.csv"))
-
-col <- substring(x[3,1],11,nchar(x[3,1]))
-colnames <- unlist(strsplit(col,", "))
-x2 <- na.omit(x)
-colnames(x2) <- colnames
-write.csv(x2,"output.csv",row.names = FALSE)
+x <- read.csv('all_edit_owndb.csv',header = F)
+col <- unlist(c(x[4,]))
+col[1] <- substring(col[1],11,nchar(col[1]))
+names(col) <- NULL
+x2 <- read.csv('all_edit_owndb.csv',header = F,sep = "\t",comment.char = "#")
+colnames(x2) <- col
+write.csv(x2,"RESULT_owndb_extract.csv",row.names = FALSE)
 EOF
 
-if [ "$execute" == "TRUE" ]; then
-	Rscript tidy_result.r
+cat<<EOF >extract_ncbidb_result.r
+options(stringsAsFactors = FALSE)
+x <- read.csv('all_edit_ncbidb.csv',header = F)
+col <- unlist(c(x[4,]))
+col[1] <- substring(col[1],11,nchar(col[1]))
+names(col) <- NULL
+x2 <- read.csv('all_edit_ncbidb.csv',header = F,sep = "\t",comment.char = "#")
+colnames(x2) <- col
+write.csv(x2,"RESULT_ncbidb_extract.csv",row.names = FALSE)
+EOF
+
+cat<<EOF >owndb_result.r
+options(stringsAsFactors = FALSE)
+x <- read.csv('all_owndb.csv',header = F)
+col <- unlist(c(x[4,]))
+col[1] <- substring(col[1],11,nchar(col[1]))
+names(col) <- NULL
+x2 <- read.csv('all_owndb.csv',header = F,sep = "\t",comment.char = "#")
+colnames(x2) <- col
+write.csv(x2,"RESULT_owndb.csv",row.names = FALSE)
+EOF
+
+cat<<EOF >ncbidb_result.r
+options(stringsAsFactors = FALSE)
+x <- read.csv('all_ncbidb.csv,header = F)
+col <- unlist(c(x[4,]))
+col[1] <- substring(col[1],11,nchar(col[1]))
+names(col) <- NULL
+x2 <- read.csv('all_ncbidb.csv',header = F,sep = "\t",comment.char = "#")
+colnames(x2) <- col
+write.csv(x2,"RESULT_ncbidb.csv",row.names = FALSE)
+EOF
+
+
+
+if [ "$extract" == "yes" ]; then
+  if [ "$type" == "owndb" ]; then
+	  Rscript extract_owndb_result.r
+  else 
+    Rscript extract_ncbidb_result.r
+  fi
+else 
+  if [ "$type" == "owndb" ]; then
+    Rscript owndb_result.r
+  else 
+    Rscript ncbidb_result.r
+  fi
 fi
 
